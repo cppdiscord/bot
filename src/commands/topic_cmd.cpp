97fceed989ee3fd9a4bdca625014e0907a8924cd
fellalli <fellalli@web.de>
2024-03-01 14:18:35 +0100

    added /topic and /coding command

---
+++ src/commands/topic_cmd.cpp
@@ -0,0 +1,14 @@
+#include "commands.h"
+
+void cmd::topicCommand(const dpp::cluster& bot, const dpp::slashcommand_t& event)
+{
+	static int index;
+	std::string topic = cmd::utils::readFileLine("res/topic.txt", index);
+
+	dpp::embed embed = dpp::embed()
+		.set_color(COLOR_DEFAULT)
+		.add_field(topic, "");
+
+	dpp::message message(event.command.channel_id, embed);
+	event.reply(message);
+}
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <functional>
+#include <fstream>
+#include <dpp/dpp.h>
+
+#include "commands/commands.h"
+
+using json = nlohmann::json;
+
+std::list<cmdStruct> cmdList = {
+	{ "topic", "Get a topic question", cmd::topicCommand },
+	{ "coding", "Get a coding question", cmd::codingCommand }
+};
+
+int main()
+{
+	std::ifstream configFile("config.json");
+	json config = json::parse(configFile);
+
+	dpp::cluster bot(config["token"]);
+
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+		std::cout << "[!] Bot ready" << std::endl;
+
+		if (dpp::run_once<struct bulkRegister>())
+		{
+			std::vector<dpp::slashcommand> slashcommands;
+			for (const auto& item : cmdList)
+			{
+				dpp::slashcommand slashcommand;
+				slashcommand.set_name(item.name);
+				slashcommand.set_description(item.desc);
+				slashcommand.set_application_id(bot.me.id);
+				slashcommands.push_back(slashcommand);
+			}
+			bot.global_bulk_command_create(slashcommands);
+		}
+	});
+
+	bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
+		for (const auto& item : cmdList)
+		{
+			if (item.name == event.command.get_command_name())
+			{
+				item.function(bot, event);
+				return;
+			}
+		}
+	});
+
+	bot.start(dpp::st_wait);
+	return 0;
+}
